c8af7cf0bbe55baabb785383c9b83527379ece3f
Rakib <rakib@think-engineer.com>
2024-01-19 00:36:33 +0000

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function App() {
-  return <div className="bg-red-500">App</div>;
+  return <div className="bg-red-500 text-2xl text-center">App</div>;
 }
